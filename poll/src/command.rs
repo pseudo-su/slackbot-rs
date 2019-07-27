b495e62db5e5f75170222184bc45d979a694f09b
John Stableford <john.stableford@digio.com.au>
2019-07-27 20:01:59 +1000

    wip: start implementing something

--- poll/src/command.rs
+++ poll/src/command.rs
@@ -1,40 +1,75 @@
-use lambda_http::{lambda, IntoResponse, Request};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use http::header::CONTENT_TYPE;
+use lambda_http::{lambda, IntoResponse, Request, Response, Body};
+// use lambda_http::body::{Body};
 use lambda_runtime::{error::HandlerError, Context};
-use serde_json::json;
+use simple_logger;
 
-fn main() {
-    lambda!(handler)
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda!(handler);
+
+    Ok(())
 }
 
-fn handler(_: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
-    // `serde_json::Values` impl `IntoResponse` by default
-    // creating an application/json response
-    
-    Ok(
-        json!({
-        "response_type": "in_channel",
-        "replace_original": false,
-        "text": "Hello Slack! attachment",
-        "attachments": [
-            {
-                "callback_id": "1234",
-                "text": "test",
-                "fallback": "attachments not supported",
-                "actions": [{
-                    "name": "poll-action",
-                    "type": "button",
-                    "text": "Delete",
-                    "style": "danger",
-                    "value": "delete",
-                    // "confirm": {
-                    //     "title": "Delete Poll?",
-                    //     "text": "This poll will be completely removed, are you sure?",
-                    //     "ok_text": "Yes",
-                    //     "dismiss_text": "No",
-                    // },
-                }],
-            }
-            
-        ]
-    }))
+#[derive(Serialize)]
+pub struct Poll {
+    text: String,
+}
+
+impl IntoResponse for Poll {
+    fn into_response(self) -> Response<Body> {
+        Response::builder()
+            .header(CONTENT_TYPE, "application/json")
+            .body(
+                serde_json::to_string(&self)
+                    .expect("unable to serialize serde_json::Value")
+                    .into(),
+            )
+            .unwrap()
+    }
+}
+
+struct CommandEvent {
+    command: String,
+    user_id: String,
+    team_id: String,
+    channel_id: String,
+    text: String,
+}
+
+impl From<Request> for CommandEvent {
+    fn from(req: Request) -> Self {
+        CommandEvent{
+            command: "".to_owned(),
+            user_id: "".to_owned(),
+            team_id: "".to_owned(),
+            channel_id: "".to_owned(),
+            text: "".to_owned() 
+        }
+    }
+}
+
+fn handler(req: Request, c: Context) -> Result<impl IntoResponse, HandlerError> {
+    // 1. parse input string (e.text)
+    // 2. create a poll object from input str
+    // 3. Persist poll state
+    // 4. return formatted poll JSON payload to user
+
+    let ev: CommandEvent = req.into();
+    let poll = Poll{ text: ev.text };
+
+    Ok(poll)
+
+    // Ok(match e.query_string_parameters().get("first_name") {
+    //     Some(first_name) => format!("Hello, {}!", first_name).into_response(),
+    //     _ => {
+    //         error!("Empty first name in request {}", c.aws_request_id);
+    //         Response::builder()
+    //             .status(400)
+    //             .body("Empty first name".into())
+    //             .expect("failed to render response")
+    //     }
+    // })
 }
--- poll/src/interaction.rs
+++ poll/src/interaction.rs
@@ -1,20 +1,43 @@
-use lambda_http::{lambda, IntoResponse, Request};
-use lambda_runtime::{error::HandlerError, Context};
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 
-fn main() {
-    lambda!(handler)
+use lambda_runtime::{error::HandlerError, lambda, Context};
+// use log::{error};
+use simple_logger;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda!(handler);
+
+    Ok(())
+}
+
+#[derive(Serialize)]
+pub struct Poll {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct SlackInteractions {
+    name: String,
+    value: String,
+}
+
+#[derive(Deserialize)]
+struct InteractionEvent {
+    #[serde(rename = "actions")]
+    interactions: Vec<SlackInteractions>,
 }
 
 fn handler(
-    _: Request,
-    _: Context,
-) -> Result<impl IntoResponse, HandlerError> {
+    e: InteractionEvent, _: Context
+) -> Result<Poll, HandlerError> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
-    Ok(json!({
-        "response_type": "in_channel",
-        "replace_original": false,
-        "text": "You pressed a button"
-    }))
+    
+    let poll = Poll{ text: "'Do you like the thing?' Y N".to_owned() };
+
+    // `serde_json::Values` impl `IntoResponse` by default
+    // creating an application/json response
+    Ok(poll)
 }
